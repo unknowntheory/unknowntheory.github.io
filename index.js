19cab89062449cf4abeeac860ed7dd037f954ad2
Daniel London <theryy408@gmail.com>
2018-06-11 19:13:45 -0700

    fixed some images path

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ if(target==='card3'){
 function image(){
   return{
     card1:['blackbook-1-copy2.png','blackbook-2-use.PNG','blackbook-3-use.PNG'],
-    card2:['incentive-1.PNG','incentive-2-copy.PNG','incentive-3-copy.PNG'],
+    card2:['incentive-1.PNG','unknowntheory.github.io/incentive-2-copy.png','unknowntheory.github.io/incentive-3-copy.png'],
     card3:['recipe-1-use.PNG','recipe-2-use.PNG','recipe-3-use.PNG'],
   }
 }
