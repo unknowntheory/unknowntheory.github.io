b1d0e7fb81591c647ca5bafdf35dd63bfd916876
Andrew London <alnano2@gmail.com>
2018-08-13 00:44:26 -0700

    under construction: removed some projects and edited js file to reflect the change

--- index.js
+++ index.js
@@ -1,46 +1,56 @@
-const card1 = document.querySelector('.card1')
-const card2 = document.querySelector('.card2')
-const card3 = document.querySelector('.card3')
+const card1 = document.querySelector(".card1");
+const card2 = document.querySelector(".card2");
+const card3 = document.querySelector(".card3");
 let count = 0;
-function handleImageClick(e){
- let target = e.target.className
- // console.log(e)
- const images = image()
- // console.log(target);
- if(target==='card1'){
-   const cardOne = images.card1
-  if(count >= cardOne.length){
-    count = 0
+function handleImageClick(e) {
+  let target = e.target.className;
+  // console.log(e)
+  const images = image();
+  // console.log(target);
+  if (target === "card1") {
+    const cardOne = images.card1;
+    if (count >= cardOne.length) {
+      count = 0;
+    }
+    card1.src = cardOne[count];
+    count++;
   }
-  card1.src = cardOne[count]
-  count++
-}
-if(target==='card2'){
-  const cardTwo = images.card2
- if(count >= cardTwo.length){
-   count = 0
- }
- card2.src = cardTwo[count]
- count++
-}
-if(target==='card3'){
-  const cardThree = images.card3
- if(count >= cardThree.length){
-   count = 0
- }
- card3.src = cardThree[count]
- count++
-}
-
-
-}
-function image(){
-  return{
-    card1:['https://imgur.com/n1I3Hbj.png','https://imgur.com/zWrZFZH.png','blackbook-1-copy2.png'],
-    card2:['https://imgur.com/n9NAyBp.png','https://imgur.com/55E5WMA.png','incentive-1.PNG',],
-    card3:['https://imgur.com/Fc4lgqK.png','https://imgur.com/GcJ0UuV.png','https://imgur.com/e5YJxfZ.png'],
+  if (target === "card2") {
+    const cardTwo = images.card2;
+    if (count >= cardTwo.length) {
+      count = 0;
+    }
+    card2.src = cardTwo[count];
+    count++;
+  }
+  if (target === "card3") {
+    const cardThree = images.card3;
+    if (count >= cardThree.length) {
+      count = 0;
+    }
+    card3.src = cardThree[count];
+    count++;
   }
 }
-card1.addEventListener('click',handleImageClick);
-card2.addEventListener('click',handleImageClick);
-card3.addEventListener('click',handleImageClick);
+function image() {
+  return {
+    card1: [
+      "https://imgur.com/n1I3Hbj.png",
+      "https://imgur.com/zWrZFZH.png",
+      "blackbook-1-copy2.png"
+    ],
+    card2: [
+      "https://imgur.com/n9NAyBp.png",
+      "https://imgur.com/55E5WMA.png",
+      "incentive-1.PNG"
+    ],
+    card3: [
+      "https://imgur.com/Fc4lgqK.png",
+      "https://imgur.com/GcJ0UuV.png",
+      "https://imgur.com/e5YJxfZ.png"
+    ]
+  };
+}
+// card1.addEventListener('click',handleImageClick);
+card2.addEventListener("click", handleImageClick);
+// card3.addEventListener('click',handleImageClick);
